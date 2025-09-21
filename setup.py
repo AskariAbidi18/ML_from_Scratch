729a60a55aa6fd5d64aefd10a57fd5ec3466c023
Syed Mohammad Askari Abidi <askari.abidi.2005@gmail.com>
2025-09-21 11:09:11 +0530

    added label encoder

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="baremetalml",         # Package name
+    version="0.1",              # Version number
+    packages=find_packages(),   # Automatically find all subpackages
+)
