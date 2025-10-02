a94af8f1615ce4acb4c14fdc0f319ef8219b6bd3
Syed Mohammad Askari Abidi <askari.abidi.2005@gmail.com>
2025-10-03 03:47:05 +0530

    completed classification and regression metrics

--- baremetalml/metrics/classification.py
+++ baremetalml/metrics/classification.py
@@ -1,14 +1,59 @@
+import numpy as np
+
 def accuracy(y_true, y_pred):
-    pass
+    TP = TN = FP = FN = 0
+    for true, pred in zip(y_true, y_pred):
+        if true and pred:
+            TP += 1
+        elif not true and not pred:
+            TN += 1
+        elif not true and pred:
+            FP += 1
+        elif true and not pred:
+            FN += 1
+    return (TP + TN) / len(y_true)
 
 def precision(y_true, y_pred):
-    pass
+    TP = FP = 0
+    for true, pred in zip(y_true, y_pred):
+        if true and pred:
+            TP += 1
+        elif not true and pred:
+            FP += 1
+    return TP / (TP + FP) if (TP + FP) > 0 else 0
 
 def recall(y_true, y_pred):
-    pass
+    TP = FN = 0
+    for true, pred in zip(y_true, y_pred):
+        if true and pred:
+            TP += 1
+        elif true and not pred:
+            FN += 1
+    return TP / (TP + FN) if (TP + FN) > 0 else 0
 
 def f1_score(y_true, y_pred):
-    pass
+    TP = FP = FN = 0
+    for true, pred in zip(y_true, y_pred):
+        if true and pred:
+            TP += 1
+        elif not true and pred:
+            FP += 1
+        elif true and not pred:
+            FN += 1
+    precision_val = TP / (TP + FP) if (TP + FP) > 0 else 0
+    recall_val = TP / (TP + FN) if (TP + FN) > 0 else 0
+    return 2 * (precision_val * recall_val) / (precision_val + recall_val) if (precision_val + recall_val) > 0 else 0
 
 def confusion_matrix(y_true, y_pred):
-    pass
+    TP = TN = FP = FN = 0
+    for true, pred in zip(y_true, y_pred):
+        if true and pred:
+            TP += 1
+        elif not true and not pred:
+            TN += 1
+        elif not true and pred:
+            FP += 1
+        elif true and not pred:
+            FN += 1
+    return np.array([[TP, FP],
+                     [FN, TN]])
--- baremetalml/metrics/regression.py
+++ baremetalml/metrics/regression.py
@@ -1,11 +1,12 @@
+import numpy as np
 def MAE(y_true, y_pred):
-    pass
+    return np.sum(np.abs(y_true - y_pred)) / len(y_true)
 
 def MSE(y_true, y_pred):
-    pass
+    return np.sum((y_true - y_pred) **2) / len(y_true)
 
 def RMSE(y_true, y_pred):
-    pass
+    return np.sqrt(np.sum((y_true - y_pred) **2) / len(y_true))
 
 def R2(y_true, y_pred):
-    pass
+    return 1 - (np.sum((y_true - y_pred)**2) / np.sum((y_true - np.mean(y_true))**2))
